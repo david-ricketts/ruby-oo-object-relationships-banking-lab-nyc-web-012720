025a45cc1e8b0574683daf4335f380e76a89a55d
David Ricketts <david.ricketts119@gmail.com>
2020-01-31 16:33:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -52,7 +52,7 @@ class Transfer
       self.sender.balance += reverse_amount
       self.receiver.balance -= reverse_amount
       self.status = "reversed"
-      binding
+      binding.pry
       end
       
       
