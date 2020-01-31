44e7509fde15153b2a45043b3a302e862aba1a06
David Ricketts <david.ricketts119@gmail.com>
2020-01-31 15:57:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if self.valid? && @@account <= 1
+    if self.valid? && @@account <= 1 && self.sender.balance >= sel.ammount
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       @@account += 1
