f74ffec3d1554e46ab23faf87349fa8357a0114d
David Ricketts <david.ricketts119@gmail.com>
2020-01-31 15:28:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,10 @@
 class Transfer
   # your code here
-  attr_accessor :name, :other_name ,:balance
+  attr_accessor :name, :other_name ,:amount
   
   def initialize(name, other_name, amount)
+    @name = name 
+    @other_name = other_name
+    @amount = amount
+  end 
 end
